ffca67a09e3874956f73cefe1f35f856d074984c
andyjzhong <andy.zhong@libertymutual.com>
2017-04-05 13:25:37 -0400

    Added Sign Up Feature
    Added a sign up button to HTML. Further expanded on the javascript
    necessary for creating an account by adding ui functionality.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,16 @@
 'use strict'
+// Don't worry about the linter on SignUp ...yet!
+const store = require('../store')
+
+const signUpSuccess = (data) => {
+  console.log(data)
+}
+
+const signUpFailure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure
+}
