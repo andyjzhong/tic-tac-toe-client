3018f01a77369bfa9a55bd0893be0a3d3bf7a0a4
andyjzhong <andy.zhong@libertymutual.com>
2017-04-07 11:00:01 -0400

    Added Basic winChecker Function
    Added array of win combinations. Started a function to check for whether
     a player has won.

--- assets/scripts/game-logic/game.js
+++ assets/scripts/game-logic/game.js
@@ -68,6 +68,26 @@ const updateCell = function () {
 const clickHandler = () =>
   $('.box').on('click', start)
 
+// These next few functions help determine whether or not there is a winner.
+const winChecker = function () {
+  console.log('winChecker ran!')
+}
+
+// This lists out all the possible winning combinations.
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 5],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+// Use a forEach function in order to run winChecker.
+forEach.
+
 module.exports = {
   clickHandler,
   setUpGameBoard,
