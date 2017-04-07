7aa2dabd49b1088bca1fad28270dd44a2f69c345
andyjzhong <andy.zhong@libertymutual.com>
2017-04-07 17:39:14 -0400

    Added New Winning Game Logic
    Received guidance from Jordan to help build out indicator for winning
    the game. Created a new gameBoard array which logs the X's and O's. It
    then checks on the position of the array and whether or not it contains
    an X. Will need to further build this out for Columns and Diagonals, as
    well as for Player O wins.

--- assets/scripts/game-logic/game.js
+++ assets/scripts/game-logic/game.js
@@ -31,6 +31,7 @@ const gameCellIds = [
 ]
 
 // This creates the game object for logging the user
+/*
 const gameObject = {
   'game': {
     'id': 1,
@@ -43,6 +44,7 @@ const gameObject = {
     'player_o': null
   }
 }
+*/
 
 // This adds an event listener every time a box is clicked, run the updateCell function.
 const setUpGameBoard = function () {
@@ -59,10 +61,10 @@ const updateCell = function () {
   console.log(currentPlayer)
   const id = this.id
   console.log('ID of clicked box = ' + id)
-  const index = id.split('-')
   // const index = index1[1]
-  gameObject.game.cells[index] = currentPlayer
-  console.log('gameObject', gameObject)
+  gameBoard[id] = currentPlayer
+  console.log('gameBoard', gameBoard)
+  checkWinner()
 }
 
 const clickHandler = () =>
@@ -75,6 +77,56 @@ const clearBoard = function () {
 
 const resetHandler = () =>
   $('#reset-board').on('click', clearBoard)
+
+// This lists out all the possible winning combinations.
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 5],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const threeInARow = function () {
+
+}
+
+// Array to check win status. Build array during game.
+const gameBoard = new Array(9)
+
+// To see if X has won any of the rows.
+const xWinsRow = function () {
+// This checks each of the rows to see if 3 cells in a row are filled with X's.
+  if ((gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') ||
+  (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') ||
+  (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X')) {
+    console.log('True')
+  }
+}
+
+// Requirements for X to win.
+const xWins = function () {
+  return xWinsRow()
+  // || xWinsColumns() || xWinsDiagonal()
+}
+
+// Requirements for O to win.
+const oWins = function () {}
+
+// Function to check if either X or O is a winner.
+const checkWinner = function () {
+  console.log('checkWinner ran!')
+  if (xWins()) {
+    return 'X'
+  }
+  if (oWins()) {
+    return 'O'
+  }
+  return null
+}
 
 module.exports = {
   clickHandler,
