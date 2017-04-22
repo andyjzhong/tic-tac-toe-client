d72a51ecaa40a950f6dda81a065679399d037551
andyjzhong <andy.zhong@libertymutual.com>
2017-04-22 14:52:41 -0400

    Switching back to Prod URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,19 +1,19 @@
 'use strict'
 
 // Production
-// const config = {
-//   apiOrigins: {
-//     production: 'https://aqueous-atoll-85096.herokuapp.com/'
-//   }
-// }
-
-module.exports = config
-
-// Dev
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
   }
 }
 
 module.exports = config
+
+// Dev
+// const config = {
+//   apiOrigins: {
+//     production: 'https://ga-wdi-boston.herokuapp.com'
+//   }
+// }
+//
+// module.exports = config
